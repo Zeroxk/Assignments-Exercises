4a53c594ea02ef8469f45ae923fca2b0b59d5343
dhu009 <dhu009@student.uib.no>
2013-10-25 18:17:21 +0200

    Reading input

--- INF244/Project1/main.py
+++ INF244/Project1/main.py
@@ -1,29 +1,96 @@
 import os
+import random
+import math
 
-f = open('MP.txt')
-noVars = int(f.readline())
-noStateVars = int(f.readline())
-noFuncs = int(f.readline())
+global noIterations,noIndepDec,codeRate,bitEnergy,noise,root,msgPassVariant
+
+#Generate random numbers according to gaussian distribution using box-muller method
+def genAWGN():
+
+    while True:
+        u = random.uniform(-1,1)
+        v = random.uniform(-1,1)
+        ##print "u:" + str(u) + "v:" + str(v)
+        
+        s = u**2 + v**2
+        ##print "s:" + str(s)
+
+        z0 = 0
+        z1 = 0
+        if not (s == 0 or s == 1 or s > 1):
+            z0 = u * math.sqrt((-2*math.log(s)/s))
+            z1 = v * math.sqrt((-2*math.log(s)/s))
+            break
+
+        #print str(z0) + " " + str(z1)
+
+    #We only need one sample so we just pick z0
+    return z0
+
+###################
+#####  MAIN  ######
+###################
+
+with open('MP.txt') as f:
+    lines = []
+    for line in f:
+        lines.append(line)
+    lines = [line.rstrip('\n') for line in lines]
+
+noVars = int(lines[0])
+noStateVars = int(lines[1])
+noFuncs = int(lines[2])
+
+lines = lines[3:]
+#print lines
 funcs = []
 for i in xrange(0,noFuncs):
-    funcs.append(f.readline())
-decMode = f.readline()
+    funcs.append(lines[i])
+
+funcs = [(x.split(']')[0].lstrip('['),x.split(']')[1]) for x in funcs] 
+print funcs
+
+boolFuncTT = []
+singleVars = []
+for i in funcs:
+#print i
+    if i[1] == "G":
+        singleVars.append(int(i[0]))
+    else:
+        tmp = i[0].split(',')
+        tmp.append(i[1])
+        boolFuncTT.append(tmp)
+
+print boolFuncTT
+print singleVars
+
+decMode = lines[noFuncs]
+
+print "DecMode is: " + decMode
+
 if decMode == "A":
-    noIterations = f.readline()
-    noIndDec = f.readline()
-    codeRate = f.readline()
-    bitEnergy = f.readline()
-    noise = f.readline()
+    noIterations = int(lines[noFuncs+1])
+    noIndepDec = int(lines[noFuncs+2])
+    codeRate = float(lines[noFuncs+3])
+    bitEnergy = float(lines[noFuncs+4])
+    noise = float(lines[noFuncs+5])
 elif decMode == "B":
-    root = f.readline()
+    root = int(lines[noFuncs+1])
 elif decMode == "C":
-    variant = f.readline()
-    noIndDec = f.readline()
-    codeRate = f.readline()
-    bitEnergy = f.readline()
-    noise = f.readline()
-f.close()
+    msgPassVariant = lines[noFuncs+1]
+    noIndepDec = int(lines[noFuncs+2])
+    codeRate = float(lines[noFuncs+3])
+    bitEnergy = float(lines[noFuncs+4])
+    noise = float(lines[noFuncs+5])
+'''
 print noVars
 print noStateVars
 print noFuncs
 print funcs
+'''
+'''
+for i in range(0,100):
+    z = math.sqrt(noise/2.0)*genAWGN()
+    print z
+'''
+
