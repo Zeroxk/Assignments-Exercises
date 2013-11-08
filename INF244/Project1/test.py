bef27ab1a611aa5040b8272f148b1d6231ad14ec
dhu009 <dhu009@student.uib.no>
2013-11-08 18:19:25 +0100

    Compute marginals

--- INF244/Project1/test.py
+++ INF244/Project1/test.py
@@ -1,4 +1,5 @@
 import itertools
+import time
 
 x0 = [[0,1], [1,1]]
 x1 = [[1,2], [1,1]]
@@ -26,41 +27,67 @@ print f
 
 def genTT(n):
     l = itertools.product([0,1], repeat=n)
-    yield list(l)[::-1]
+    for i in l:
+        yield list(i)[::-1]
 
 global visitedF, visitedX
 visitedF = list(itertools.repeat(-1, len(f)))
 visitedX = list(itertools.repeat(-1, len(x)))
 
+#Flag tells whether we are in X or F list, X=0, F=1
 def dfs(curr, prev, flag):
-    print "Curr: " + str(curr) + " Prev: " + str(prev) + " Flag: " + flag
+    fc = 'X' if flag == 0 else 'F'
+    print "Curr: " + str(curr) + " Prev: " + str(prev) + " Flag: " + fc
+    
     res = []
-    if flag == 'X':
-        visitedX[curr] = 1
-        print x[curr]
-        if len(x[curr][0]) == 1:
-            return x[curr][1]
-        for i in x[curr][0]:
-            print i
-            if visitedF[i] == -1:
-                print "Visiting " + str(i)
-                res.append(dfs(i, curr, 'F'))
-            else:
-                print str(i) + " already visited"
-    elif flag == 'F':
-        visitedF[curr] = 1
-        print f[curr]
-        if len(f[curr][0]) == 1:
-                return f[curr][1]
-        for i in f[curr][0]:
-            print i
-            if visitedX[i] == -1:
-                print "Visiting " + str(i)
-                res.append(dfs(i,curr, 'X'))
-            else:
-                print str(i) + " already visited"
-    return res
+    visited = []
+    otherVis = []
+    nodes = []
+    if flag == 0:
+        visited = visitedX
+        otherVis = visitedF
+        nodes = x
+    elif flag == 1:
+        visited = visitedF
+        otherVis = visitedX
+        nodes = f
 
-print dfs(0,0,'X')
+    visited[curr] = 1
+    print nodes[curr]
+
+    if len(nodes[curr][0]) == 1 and nodes[curr][0][0] == prev:
+        print str(curr) + " has Unit marginal"
+        return nodes[curr][1]
+    for i in nodes[curr][0]:
+        print i
+        if otherVis[i] == -1:
+            print "Visiting: " + str(i)
+#time.sleep(5)
+            res.append(dfs(i,curr, (flag + 1) % 2))
+        else:
+            print str(i) + " already visited"
+    return computeMarginal(nodes[curr][0], curr, res)
+#return res
+
+print dfs(0,-1,0)
+
+def computeMarginal(neigh,node, res):
+    TT = genTT(len(neigh))
+    ind = 0
+    offset = 2**len(neigh)
+    ones = zeroes =  0
+    for i in TT:
+        prod = 1
+        for j in res:
+            prod *= j[ind]
+            ind += 1
+        indNode = neigh.index(node)
+        if indNode == 0:
+            zeroes += prod
+        else:
+            ones += prod
+    print "Marginal for " + str(node) + " is " + str([ones,zeroes])
+    
+    return [ones,zeroes]
 
 
