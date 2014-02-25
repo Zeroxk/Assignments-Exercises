5369885ad61a82ebd9e72e93dea608aeea293d51
Zeroxk <dhu009@student.uib.no>
2014-02-25 02:09:19 +0100

    Works but need to figure out what i to start check

--- INF236/Oblig1/problem1.c
+++ INF236/Oblig1/problem1.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <mpi.h>
+#include <math.h>
 
 int isPrime(int n) {
+    if(n == 0 || n == 1 || n == 2) return 0;
     int i;
-    for(i=2; i<sqrt(n); i++){
+    for(i=2; i<=(int)sqrt((double)n); i++){
         if(n%i == 0) return 0;
     }
 
@@ -14,26 +16,36 @@ int isPrime(int n) {
 
 int main(int argc, char* argv[]) {
     int rank,np,n,val,res;
-    n = 20; res = 0;
-    MPI_INIT(&argc,&argv)
-    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    n = 20; res,val = 0;
+    MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-    if(rank) {
+    if(rank == 0) {
         int i;
-        for(i=1; i<np; i++){
+        for(i=1; i<np; i++) {
+            val += (n/(np-1));
+            printf("Sending value %d\n",val);
             MPI_Send(&val,1,MPI_INT,i,1,MPI_COMM_WORLD);
         }
     }else {
-        MPI Status status;
+        MPI_Status status;
         MPI_Recv(&val,1,MPI_INT,0,1,MPI_COMM_WORLD,&status);
+        printf("Process %d received value %d\n",rank,val);
 
         int i;
         int a,b;
-        for(i=val-(n/np); i<=val-2; i+=2) {
+        for(i=(n-val); i<val; i++) {
             if(isPrime(i) && isPrime(i+2)) {
+                printf("(%d,%d)\n",i,i+2);
                 res++;
             }
         }
+        printf("%d consecutive prime pairs from process %d\n",res-1,rank);
+
     }
+
+    MPI_Finalize();
+
+    return 0;
 }
