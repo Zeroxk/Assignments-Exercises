cf6e7aa0c6e83762ac0e17e5e0192cb4ee864f68
Zeroxk <dhu009@student.uib.no>
2014-03-31 08:25:41 +0200

    More

--- INF236/Oblig2/problemA.c
+++ INF236/Oblig2/problemA.c
@@ -4,20 +4,40 @@
 
 double f(double x,int n) {
     int i,j;
-    double res;
+    double res,res2;
+    res = 0.0;
     for(i=1;i<=n;i++) {
+        res2 = 0.0;
         for(j=1;j<=n;j++) {
             double r1 = sin( (pow(x,i) + pow(x,j)/2) );
             double r2 = 1 - cos( (pow(x,i) + pow(x,j))/2 );
-            res += r1 * r2;
+            res2 += r1 * r2;
         }
+        res += res2;
     }
-    return res;
+    return -3 + res;
 }
 
 int main(int argc,char** argv) {
 
+    int rank,np;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+
+    if(argc < 3) {
+        printf("Not enough arguments, need 2, epsilon and n\n");
+        MPI_Finalize();
+        return 0;
+    }
+
+    double epsilon; int n;
+    epsilon = atof(argv[1]);
+    n = atoi(argv[2]);
+
+    if(rank == 0) {
     
+    }
 
 
     return 0;
